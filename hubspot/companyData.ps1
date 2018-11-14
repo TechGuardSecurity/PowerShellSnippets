1f5c62a3ca5950421a47e1421a5f940ba17c1609
WeatheRay <n.a.layfield@gmail.com>
2018-11-14 09:36:00 -0600

    Update companyData.ps1

--- hubspot/companyData.ps1
+++ hubspot/companyData.ps1
@@ -1,9 +1,47 @@
+######################################################
+#
+#   Techguard Security
+#   Programed by: Nathan Layfield
+#   Retrieve company list from HubSpot.  Combine list
+#   based on spcified properties and create CSV.
+#   Upload to SFTP server
+#
+######################################################
+
+#store cred file and powershell file in same directory
+
+#allow running of scripts. 
+#set-executionpolicy remotesigned
+#powershell.exe -noprofile -executionpolicy bypass -file .\script.ps1
+
+#run this as admin to install ssh
+#install-module ssh-posh
+
+
+
 $more=1
 $tab = [char]9
-$properties = ('name','contact_name','contact_email')
+$sortfield = 'eula_signed'
+$properties = ('name','eula_signed','contact_email')
 $headers=''
 $url_list =''
 $delimiter = '|'
+
+#Company Specifics:
+$remoteComputer = "192.168.1.1"
+$port = 22
+$remote = "/remote/path"
+$user = "user"
+$hapikey = "hubspot-api-key-xxxxxxxx"
+
+
+
+#create secure password file
+#read-host -assecurestring | convertfrom-securestring | out-file cred.txt
+if (!$cred){
+    $pass = get-content cred.txt | convertto-securestring
+    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $user,$pass
+}
 
 #need to set the location of the script
 $fileName = "tempFile.txt"
@@ -11,8 +49,8 @@ $csvFile = "Company Report.csv"
 
 #create url to query
 #example
-#https://api.hubapi.com/companies/v2/companies/paged?hapikey=xxxx&properties=name&limit=250
-$baseurl = "https://api.hubapi.com/companies/v2/companies/paged?hapikey=xxxx" # + "& ......
+#https://api.hubapi.com/companies/v2/companies/paged?hapikey=$hapikey&properties=name&properties=eula_signed&limit=250
+$baseurl = "https://api.hubapi.com/companies/v2/companies/paged?hapikey=$hapikey" # + "& ......
 foreach ($prop in $properties){
     #Create URL
 
@@ -44,19 +82,23 @@ while ($more){
     $URL_use = $url + "&offset=" + $data.offset
     foreach ($comp in $data.companies){
         $comp = $comp.properties
-
-        foreach ($prop in $properties){
-            foreach ($key in $comp){
-                if(!$content){
-                    $content = $key.$prop.value
+        #only include companies where specific field is used
+        if($comp.$sortfield.value -eq 'Yes'){
+            foreach ($prop in $properties){
+                foreach ($key in $comp){
+                    if(!$content){
+                        $content = $key.$prop.value
+                    }
+                    else{
+                        $content = $content + $delimiter + $key.$prop.value
+                    }
+                    #echo ($prop + "`t`t`t " + $key.$prop.value)
                 }
-                else{
-                    $content = $content + $delimiter + $key.$prop.value
-                }
-                #echo ($prop + "`t`t`t " + $key.$prop.value)
             }
             
-        }$content | out-file -filepath  $fileName -noclobber -append
+        }
+        
+        $content | out-file -filepath  $fileName -noclobber -append
         $content= ''
         #echo ''
     }    
@@ -65,3 +107,9 @@ while ($more){
 #convert txt to csv and delete txt
 import-csv $fileName -delimiter $delimiter | export-csv $csvFile -NoType
 Remove-item $fileName
+
+#SFTP connection and transfer
+New-SFTPSession -ComputerName $remoteComputer -port $port -Credential $cred
+$session =  $sftpSessions.sessionid
+Set-SFTPFile -sessionid $session -remotepath $remote -localfile test
+Remove-SFTPSession -Index $session
